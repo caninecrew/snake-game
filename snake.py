d65af045c7a1ce30cae3a86097ba672d3e3cebac
caninecrew <samuelleerumbley@gmail.com>
2025-04-10 11:57:28 -0500

    Implement Cube class with initialization, move, and draw methods

--- snake.py
+++ snake.py
@@ -4,3 +4,16 @@ import pygame
 import tkinter as tk
 from tkinter import messagebox
 
+class Cube(object):
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        self.pos = start
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
